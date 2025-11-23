abc2a418180c23fe7788bec3fefddb06fece068f
iamrafsanrafi <hm.rafsanrafi@gmail.com>
2025-11-23 23:24:54 +0600

    Added — responsive projects sec design

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -2,10 +2,15 @@ import { useGSAP } from "@gsap/react"
 import { allProjects } from "../constants/data"
 import GitHubIcon from "../icons/GitHubIcon"
 import RedirectIcon from "../icons/RedirectIcon"
-
 import gsap from "gsap"
+import { useMediaQuery } from "react-responsive"
 
 const ProjectsSection = () => {
+    // Extra responsive breakpoint
+    const breakpoint1 = useMediaQuery({ maxWidth: 480 });
+    const breakpoint2 = useMediaQuery({ minWidth: 601 });
+    const breakpoint3 = useMediaQuery({ minWidth: 1001 });
+
     useGSAP(() => {
         const timeline = gsap.timeline({
             scrollTrigger: {
@@ -45,16 +50,18 @@ const ProjectsSection = () => {
     }, []);
 
     return (
-        <section id="projects" className="max-w-[1000px] mx-auto py-[150px]">
+        <section id="projects" className="max-w-[1000px] mx-auto py-[100px] md:py-[150px]">
             {/* ---- Section Heading ---- */}
-            <div id="project-heading" className="flex items-center mt-2.5 mb-10">
-                <p className="font-mono text-xl text-[#64ffda] relative bottom-1 mr-2.5 select-none">03.</p>
-                <h3 className="font-calibre text-[32px] text-[#ccd6f6] font-semibold leading-[42px]">Some Things I've Built</h3>
-                <hr className="w-[300px] border-[#2d3952] ml-5 relative bottom-1" />
+            <div className="flex items-center mt-2.5 mb-10">
+                <p className="font-mono text-lg md:text-xl leading-[23px] md:leading-[26px] text-[#64ffda] relative bottom-1 mr-2.5 select-none">03.</p>
+
+                <h3 className="font-calibre text-2xl leading-[31px] md:text-[32px] md:leading-[42px] text-[#ccd6f6] font-semibold">Some Things I've Built</h3>
+
+                <hr className={`flex-1 ${breakpoint3 ? "max-w-[300px]" : "md:max-w-[200px]"} border-[#2d3952] ${breakpoint2 ? "ml-5" : "ml-2.5"} relative bottom-1`} />
             </div>
 
             {/* ---- Projects ---- */}
-            <div className="flex flex-col gap-y-[100px]">
+            <div className={`flex flex-col ${breakpoint2 ? "gap-y-[100px]" : "gap-y-[70px]"}`}>
                 {allProjects.map(project => (
                     <div
                         key={project.id}
@@ -64,49 +71,49 @@ const ProjectsSection = () => {
                         <a
                             href={project.link}
                             target="_blank"
-                            className="block max-w-[580px] bg-[#64ffda] rounded-[3px] hover:bg-transparent relative before:absolute before:cotent-[''] before:w-full before:h-full before:inset-0 before:z-3 before:bg-[#0a192f] before:mix-blend-screen group select-none"
+                            className={`block ${breakpoint2 ? "w-[58.33%] relative h-[380px] md:h-[360px] max-w-[580px]" : "w-full opacity-25 absolute inset-0 h-full"} bg-[#64ffda] rounded-[3px] hover:bg-transparent before:absolute before:cotent-[''] before:w-full before:h-full before:inset-0 before:z-3 before:bg-[#0a192f] before:mix-blend-screen group select-none`}
                             style={{
                                 transition: "0.25s cubic-bezier(0.645, 0.045, 0.355, 1)"
                             }}
                         >
                             <div
-                                className="rounded-[3px] grayscale contrast brightness-90 mix-blend-multiply group group-hover:grayscale-0 group-hover:brightness-100 group-hover:mix-blend-normal"
+                                className="rounded-[3px] grayscale contrast brightness-90 mix-blend-multiply group group-hover:grayscale-0 group-hover:brightness-100 group-hover:mix-blend-normal w-full h-full"
                                 style={{
                                     transition: "0.25s cubic-bezier(0.645, 0.045, 0.355, 1)"
                                 }}
                             >
-                                <img src={project.image} alt={project.name} className="w-full rounded-[3px]" />
+                                <img src={project.image} alt={project.name} className="w-full h-full rounded-[3px] object-cover object-center" />
                             </div>
                         </a>
 
 
                         {/* ---- Project Details ---- */}
-                        <div className={`absolute z-5 max-w-[495px] ${project.id % 2 == 0 ? "text-left left-0" : "text-right right-0"}`}>
+                        <div className={`${breakpoint1 ? "pt-[30px] px-[25px] pb-[20px]" : ""} ${breakpoint2 ? "absolute w-[50%] p-0" : "relative w-full"} ${!breakpoint1 && !breakpoint2 ? "pt-10 px-10 pb-[30px]" : ""} z-5 md:max-w-[495px] ${project.id % 2 == 0 ? "text-left left-0" : "text-right right-0"}`}>
                             <p className="font-mono text-[13px] text-[#64ffda] leading-[17px] my-[10px]">Featured Project</p>
 
                             <h4
-                                className="inline-block font-calibre font-semibold text-[28px] text-[#ccd6f6] hover:text-[#64ffda] mb-5 cursor-pointer"
+                                className="inline-block font-calibre font-semibold text-2xl leading-[31px] md:text-[28px] md:leading-[36px] text-[#ccd6f6] mb-5 cursor-pointer"
                                 style={{
                                     transition: "0.25s cubic-bezier(0.645, 0.045, 0.355, 1)"
                                 }}
                             >
-                                <a href={project.link} target="_blank">{project.name}</a>
+                                <a href={project.link} target="_blank" className="focus:text-[#64ffda] active:bg-[#226185] active:text-[#64ffda] hover:text-[#64ffda]">{project.name}</a>
                             </h4>
 
-                            <div
-                                className="font-calibre text-lg text-[#a8b2d1] leading-[28px] rounded-[3px] p-[25px] bg-[#172a45]"
+                            <p
+                                className={`font-calibre text-lg text-[#a8b2d1] leading-[23px] rounded-[3px] ${breakpoint2 ? "bg-[#172a45] p-[25px]" : "bg-transparent py-5"}`}
                                 style={{
-                                    boxShadow: "rgba(2, 12, 27, 0.7) 0px 10px 30px -15px"
+                                    boxShadow: `${breakpoint2 ? "rgba(2, 12, 27, 0.7) 0px 10px 30px -15px" : "none"}`
                                 }}
                             >
                                 {project.description}
-                            </div>
+                            </p>
 
                             <ul className={`flex gap-x-5 ${project.id % 2 == 0 ? "justify-start" : "justify-end"} mt-[25px] mb-2.5`}>
                                 {project.technologies.map((t, index) => (
                                     <li
                                         key={index}
-                                        className="font-mono text-[13px] text-[#8892b0] leading-[17px]"
+                                        className={`font-mono text-[13px] ${breakpoint2 ? "text-[#8892b0]" : "text-[#ccd6f6]"} leading-[17px] mb-[7px]`}
                                     >{t}</li>
                                 ))}
                             </ul>
@@ -116,7 +123,7 @@ const ProjectsSection = () => {
                                 <a
                                     href={project.github}
                                     target="_blank"
-                                    className="text-[#ccd6f6] hover:text-[#64ffda] inline-block p-2.5"
+                                    className="text-[#ccd6f6] focus:text-[#64ffda] active:bg-[#226185] active:text-[#64ffda] hover:text-[#64ffda] inline-block p-2.5"
                                     style={{
                                         transition: "0.25s cubic-bezier(0.645, 0.045, 0.355, 1)"
                                     }}
@@ -126,7 +133,7 @@ const ProjectsSection = () => {
                                 <a
                                     href={project.link}
                                     target="_blank"
-                                    className="text-[#ccd6f6] hover:text-[#64ffda] inline-block p-2.5"
+                                    className="text-[#ccd6f6] focus:text-[#64ffda] active:bg-[#226185] active:text-[#64ffda] hover:text-[#64ffda] inline-block p-2.5"
                                     style={{
                                         transition: "0.25s cubic-bezier(0.645, 0.045, 0.355, 1)"
                                     }}
